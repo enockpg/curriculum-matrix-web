01faf257252129639370f34bc7278fed6f851938
enockpinheiro <enockpinheiro@192.168.1.104>
2019-10-27 19:20:22 -0300

    router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
+import { HomeComponent } from './features/home/home.component';
+import { AppComponent } from './app.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: './features/home/home.module#HomeModule'
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
